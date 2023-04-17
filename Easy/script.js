becaa15401cd1b049f1534a4e64ebf2aea587ca4
Kevin Lam <Kevin@AdminisatorsMBP.attlocal.net>
2023-04-17 16:08:57 -0400

    easy

--- Easy/script.js
+++ Easy/script.js
@@ -1,5 +1,15 @@
-const num1 = 6;
-const num2 = 2;
-const difference = num1 - num2;
+const name1 = "Kevin";
+const name2 = "Kendrick";
+const difference = name1.length - name2.length;
 
-console.log(`The difference between ${num1} and ${num2} is ${difference}`);
+if (difference > 0) {
+  console.log(
+    `The name ${name1} is longer than ${name2} by ${difference} characters`
+  );
+} else if (difference < 0) {
+  console.log(
+    `The name ${name2} is longer than ${name1} by ${-difference} characters`
+  );
+} else {
+  console.log(`The names ${name1} and ${name2} have the same length`);
+}
