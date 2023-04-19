378f2c4083e9041718fbfb729dda98186ede20cc
Kevin Lam <Kevin@Administrators-MacBook-Pro.local>
2023-04-19 09:59:26 -0400

    final

---
+++ Hard/script.js
@@ -0,0 +1,33 @@
+// FUNCTION FUNCTION FUNCTION FUNCTION FUNCTION FUNCTION FUNCTION
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// SOLUTION SOLUTION SOLUTION SOLUTION SOLUTION SOLUTION SOLUTION SOLUTION
+const x = 10;
+const y = 5;
+
+const sum = add(x, y);
+console.log(`The sum of ${x} and ${y} is ${sum}`);
+
+const difference = subtract(x, y);
+console.log(`The difference between ${x} and ${y} is ${difference}`);
+
+const product = multiply(x, y);
+console.log(`The product of ${x} and ${y} is ${multiply}`);
+
+const quotient = divide(x, y);
+console.log(`The quotient of ${x} and ${y} is ${divide}`);
+//TYPE NODE SCRIPT.JS IN TERMINAL FOR ANSWER
